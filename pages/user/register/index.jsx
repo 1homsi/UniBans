c7a70ed639cdc67fbd2bf139d5b139e07a0dcaa7
1homsi <65892600+1homsi@users.noreply.github.com>
2022-01-28 22:46:14 +0200

    Added some files

--- pages/user/register/index.jsx
+++ pages/user/register/index.jsx
@@ -1,7 +1,9 @@
+import styles from "../../../styles/Register.module.css";
+
 const index = () => {
   return (
     <div>
-      <h1>Register</h1>
+      <h1 className={styles.info}>Register</h1>
     </div>
   );
 };
