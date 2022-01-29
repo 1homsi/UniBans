c3e6d922e80f64a40b68b40231f8949d6a481746
1homsi <65892600+1homsi@users.noreply.github.com>
2022-01-30 01:21:31 +0200

    Final Commit of the day
    idk updated some useless files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
-import 'semantic-ui-css/semantic.min.css';
-import './App.css';
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
 
-import { AuthProvider } from './context/auth';
-import AuthRoute from './util/AuthRoute';
+import { AuthProvider } from "./context/auth";
+import AuthRoute from "./util/AuthRoute";
 
-import MenuBar from './components/MenuBar';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import MenuBar from "./components/MenuBar";
+import Home from "./pages/Home";
+import Login from "./pages/User/Login";
+import Register from "./pages/User/Register";
 
 function App() {
   return (
