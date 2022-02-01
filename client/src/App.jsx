91bfe9f64ffc89defce99d329bf4d15d9ccdb742
1homsi <65892600+1homsi@users.noreply.github.com>
2022-02-01 09:37:22 +0200

    Added Chat server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,39 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 
 import "semantic-ui-css/semantic.min.css";
 import "./App.scss";
 
-import { AuthProvider } from "./context/auth";
+import { AuthProvider, AuthContext } from "./context/auth";
 import AuthRoute from "./util/AuthRoute";
 
 import MenuBar from "./components/Menu/MenuBar";
 import Home from "./pages/Home";
 import Login from "./pages/User/Login";
 import Register from "./pages/User/Register";
+import Chat from "./pages/Chat/chat";
 
 function App() {
+  const { user } = useContext(AuthContext);
+  let loc = "";
+  if (user) {
+    loc = `/chat/${user.username}/combo`;
+    console.log(user.username);
+  } else {
+    loc = `/chat/Guest/combo`;
+  }
+
   return (
     <AuthProvider>
       <Router>
         <Container>
           <MenuBar />
+          <Link to={loc}>lol</Link>
           <Route exact path="/" component={Home} />
           <AuthRoute exact path="/login" component={Login} />
           <AuthRoute exact path="/register" component={Register} />
+          <AuthRoute path="/chat/:name/:room" component={Chat} />
         </Container>
       </Router>
     </AuthProvider>
