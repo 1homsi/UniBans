de9611aa41e4bba87ccd6876949c2706855b3f76
mohamadfaki <mohamadfaki3@gmail.com>
2022-02-04 20:27:38 +0200

    about page done

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import "./About.scss";
+import ReactLogo from '../images/image.svg';
 
 function About() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="main">
+      <div className="section">
+        <div className="container">
+          <div className="content-section">
+            <div className="title">
+              <h1>About Us</h1>
+            </div>
+            <div className="content">
+              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus facilis aliquam voluptatum animi autem omnis illum, id perspiciatis magni magnam! Saepe eaque accusamus dignissimos qui quibusdam quas, officia voluptatem esse!</p>
+              <div className="button">
+                <a href="">Read More</a>
+              </div>
+            </div>
+            <div className="social">
+              <a href=""><i className="fab fa-facebook-f"></i></a>
+              <a href=""><i className="fab fa-twitter"></i></a>
+              <a href=""><i className="fab fa-instagram"></i></a>
+            </div>
+          </div>
+          <div className="image-section">
+            <img src={ReactLogo} alt="" />
+          </div>
+        </div>
+    </div>
     </div>
   );
 }
