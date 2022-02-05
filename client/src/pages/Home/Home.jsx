68c9a7f7a9ab8aed321c20f069641fd8479a8e28
mohamadfaki <mohamadfaki3@gmail.com>
2022-02-05 21:08:54 +0200

    worked on home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,27 +1,43 @@
 import React from "react";
+import "./Home.scss";
+import 'font-awesome/css/font-awesome.min.css';
 
 function Home() {
   return (
     <div>
       <h1 className="Content"></h1>
-        <select name="threads" id="" className="find-threads">
+      <i class="fas fa-comment"></i>
+        <select name="threads" id="" className="find-threads options">
           <option value="your_threads">Your Threads</option>
           <option value="with_your_posts">Threads with your posts</option>
           <option value="unanswered">Unanswered Threads</option>
         </select>
-        <select name="watched" id="" className="whatched">
+        <i class="fas fa-bell"></i>
+        <select name="watched" id="" className="whatched options">
           <option value="threads">Watched Threads</option>
           <option value="forums">Watched Forums</option>
         </select>
-        <a link to=""><i></i>Mark Forums Read</a>
+        <a link to=""> <i class="fas fa-eye-slash"></i>Mark Forums Read</a>
         <div className="notes" id="div-trans">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat aliquam perferendis at aliquid neque voluptatibus corporis repellendus esse dolor facere magnam nihil autem omnis quibusdam, aperiam delectus vel! Vero, non.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Placeat aliquam perferendis at aliquid neque voluptatibus
+             corporis repellendus esse dolor facere magnam nihil 
+            autem omnis quibusdam, aperiam delectus vel! Vero, non.</p>
         </div>
         <div className="notes" id="div-blue">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat aliquam perferendis at aliquid neque voluptatibus corporis repellendus esse dolor facere magnam nihil autem omnis quibusdam, aperiam delectus vel! Vero, non.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Placeat aliquam perferendis at aliquid neque voluptatibus
+             corporis repellendus esse dolor facere magnam nihil
+             autem omnis quibusdam, aperiam delectus vel! Vero, non.</p>
         </div>
         <div className="notes" id="div-red">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat aliquam perferendis at aliquid neque voluptatibus corporis repellendus esse dolor facere magnam nihil autem omnis quibusdam, aperiam delectus vel! Vero, non.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Placeat aliquam perferendis at aliquid neque voluptatibus 
+            corporis repellendus esse dolor facere magnam nihil 
+            autem omnis quibusdam, aperiam delectus vel! Vero, non.</p>
+        </div>
+        <div className="">
+
         </div>
     </div>
   );
