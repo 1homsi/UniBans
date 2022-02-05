a211c5c7af98acbe5a07561efdaa8628fc6051e3
mohamadfaki <mohamadfaki3@gmail.com>
2022-02-05 21:47:16 +0200

    worked on home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -4,20 +4,23 @@ import 'font-awesome/css/font-awesome.min.css';
 
 function Home() {
   return (
-    <div>
-      <h1 className="Content"></h1>
+    <div className="main">
+      <div className="options">
       <i class="fas fa-comment"></i>
         <select name="threads" id="" className="find-threads options">
           <option value="your_threads">Your Threads</option>
           <option value="with_your_posts">Threads with your posts</option>
           <option value="unanswered">Unanswered Threads</option>
         </select>
+      </div>
+      <div className="options">
         <i class="fas fa-bell"></i>
         <select name="watched" id="" className="whatched options">
           <option value="threads">Watched Threads</option>
           <option value="forums">Watched Forums</option>
         </select>
-        <a link to=""> <i class="fas fa-eye-slash"></i>Mark Forums Read</a>
+      </div>
+        <a link to="" className="options"> <i class="fas fa-eye-slash"></i>Mark Forums Read</a>
         <div className="notes" id="div-trans">
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
             Placeat aliquam perferendis at aliquid neque voluptatibus
